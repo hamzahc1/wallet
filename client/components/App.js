ed1e74c74594023c9de173995991ed786db90b5d
Hamzah Chaudhary <hamzahchaudhary@gmail.com>
2016-07-12 16:35:32 +0100

    Add form control for checking if amount entered is greater than current balance

--- client/components/App.js
+++ client/components/App.js
@@ -3,15 +3,18 @@ import React from 'react';
 import Header from './Header';
 import AccountHistory from './AccountHistory';
 import AccountItem from './AccountItem';
+import Transaction from './Transaction'
 
 export default class App extends React.Component {
 
   render(){
     let x = [{'amount': 10, 'date':'today'}, {'amount': 20, 'date':'yesterday'}];
+    let y = 10;
     return(
     <div>
     <Header />
     <AccountHistory history = {x} />
+    <Transaction balance = {y} />
     </div>
     )
   }
--- client/components/Transaction.js
+++ client/components/Transaction.js
@@ -1,2 +1,41 @@
 import React from 'react';
+import {FormGroup, FormControl} from 'react-bootstrap';
 
+export default class Transaction extends React.Component{
+  constructor(props) {
+    super(props);
+    this.state = {
+      depositValue: 0
+    }
+  }
+
+  getValidationState() {
+    const balance = this.props.balance;
+    if (this.state.depositValue < balance) return 'success';
+    else if (this.state.depositValue === balance) return 'warning';
+    else if (this.state.depositValue > balance) return 'error';
+  }
+
+  handleChange = (e) => {
+    this.setState({ depositValue: e.target.value });
+  }
+
+  render() {
+    return (
+      <form>
+        <FormGroup
+          controlId="formBasicText"
+          validationState={this.getValidationState()}
+        >
+          <FormControl
+            type="number"
+            value={this.state.depositValue}
+            placeholder="Enter amount"
+            onChange={this.handleChange}
+          />
+          <FormControl.Feedback />
+        </FormGroup>
+      </form>
+    );
+  }
+};
