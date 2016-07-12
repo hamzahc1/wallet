4a9bc75db3c238656c41bb1733accedb3e3b1bd3
Hamzah Chaudhary <hamzahchaudhary@gmail.com>
2016-07-12 02:13:09 +0100

    Update index.js file with new App component

--- client/index.js
+++ client/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Header from './components/Header.js'
+import App from './components/App'
 
 const app = document.getElementById('app');
 
-ReactDOM.render(<Header/>, app);
+ReactDOM.render(<App />, app);
