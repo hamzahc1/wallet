b9b4c5d1fa5e06f48dbc05f5af4bc7203801053d
Hamzah Chaudhary <hamzahchaudhary@gmail.com>
2016-07-12 01:48:17 +0100

    Create basic menu buttons with link to github repo

--- client/components/Header.js
+++ client/components/Header.js
@@ -3,7 +3,11 @@ import React from 'react';
 export default class Layout extends React.Component {
   render(){
     return (
-      <h1>Wallet application!</h1>
+      <div>
+      <h1>Home</h1>
+      <h1>Reset</h1>
+      <h1> <a href = 'https://github.com/hamzahc1/wallet'>Source</a></h1>
+      </div>
       )
   }
 }
