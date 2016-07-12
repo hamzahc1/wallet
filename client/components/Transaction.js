c09623257f75252403cc0ca3c5d1228e9797ee7d
Hamzah Chaudhary <hamzahchaudhary@gmail.com>
2016-07-12 17:33:16 +0100

    Disable button if amount entered is larger than total balance

--- client/components/Transaction.js
+++ client/components/Transaction.js
@@ -1,20 +1,38 @@
 import React from 'react';
-import {FormGroup, FormControl} from 'react-bootstrap';
+import {FormGroup, FormControl, Button} from 'react-bootstrap';
 
 export default class Transaction extends React.Component{
   constructor(props) {
     super(props);
     this.state = {
-      depositValue: 0
+      depositValue: ''
     }
   }
 
   getValidationState() {
     const balance = this.props.balance;
-    if (this.state.depositValue < balance) return 'success';
-    else if (this.state.depositValue === balance) return 'warning';
-    else if (this.state.depositValue > balance) return 'error';
+    if (this.state.depositValue <= balance) {
+      return 'success';
+    }
+    else if (this.state.depositValue === balance) {
+      return 'warning';
+    }
+    else if (this.state.depositValue > balance) {
+      return 'error';
+    }
   }
+
+  // isValidAmount = () => {
+  //   if (this.state.depositValue <= this.props.balance) {
+  //     this.setState({validAmount: true});
+  //   }
+  //   else if (this.state.depositValue === this.props.balance) {
+  //     this.setState({validAmount: true});
+  //   }
+  //   else if (this.state.depositValue > this.props.balance) {
+  //     this.setState({validAmount: false});
+  //   }
+  // }
 
   handleChange = (e) => {
     this.setState({ depositValue: e.target.value });
@@ -28,13 +46,15 @@ export default class Transaction extends React.Component{
           validationState={this.getValidationState()}
         >
           <FormControl
-            type="number"
+            type="text"
             value={this.state.depositValue}
             placeholder="Enter amount"
             onChange={this.handleChange}
           />
           <FormControl.Feedback />
         </FormGroup>
+        <Button href ='#' disabled ={this.getValidationState() === 'success' ? false : true} onClick ={()=>{console.log("hellow")}}>Add</Button>
+        <Button href ='#'>Withdraw</Button>
       </form>
     );
   }
